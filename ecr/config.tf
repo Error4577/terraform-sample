01d4b8b9b94b388a47565648a3bfe26695917cc5
Error4577 <donttreadonme4577@gmail.com>
2019-12-18 18:13:01 +0900

    Create config.tf

---
+++ ecr/config.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    becket = "my-sample-terraform-ishihara"
+    key    = "terraform-sample/ecr/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
