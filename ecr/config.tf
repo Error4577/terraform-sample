dded6e718717e66eab200962e48a4afe63b29275
Error4577 <donttreadonme4577@gmail.com>
2019-12-18 18:15:16 +0900

    Update config.tf

--- ecr/config.tf
+++ ecr/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    backet = "my-sample-terraform-ishihara"
+    bucket = "my-sample-terraform-ishihara"
     key    = "terraform-sample/ecr/terraform.tfstate"
     region = "ap-northeast-1"
   }
