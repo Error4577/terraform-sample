3badb8554aecc39427358040abfb5f222644a473
Error4577 <donttreadonme4577@gmail.com>
2019-12-19 21:15:41 +0900

    Update config.tf

--- service/config.tf
+++ service/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-sample-terraform-ishihara"
-    key    = "sample/service/terraform.tfstate"
+    key    = "terraform-sample/service/terraform.tfstate"
     region = "ap-northeast-1"
   }
 }
