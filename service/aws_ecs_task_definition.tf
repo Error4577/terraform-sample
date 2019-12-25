c5a59eae1c78d5e2a1fb696e91bbb740e6a4d45c
Error4577 <donttreadonme4577@gmail.com>
2019-12-25 21:34:29 +0900

    Update aws_ecs_task_definition.tf

--- service/aws_ecs_task_definition.tf
+++ service/aws_ecs_task_definition.tf
@@ -1,12 +1,12 @@
 resource "aws_ecs_task_definition" "sample-task" {
   family                = "webapp-service"
   container_definitions = file("./container_definitions/service.json.tpl")
-  task_role_arn         = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
+  execution_role_arn    = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
   network_mode          = "bridge"
 }
 
 resource "aws_ecs_task_definition" "sample-migration-task" {
-  family                = "webapp-service"
+  family                = "webapp-migration"
   container_definitions = file("./container_definitions/migration.json.tpl")
   execution_role_arn    = data.terraform_remote_state.aws_iam.outputs.ecs_task_role_arn
   network_mode          = "bridge"
